c5f84657c54f88fb1ca144690b851d838e1248e7
Bea <samuelsson.bea@gmail.com>
2023-03-02 14:47:32 +0800

    Upscale Start

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import threading
 import cv2
 import modules
 
-inputFile = "test-footage/test.mp4"
-
+inputFile = "test-footage/test_Trim.mp4"
+scale = 4
 length = modules.vidLen(inputFile)
 
 threads = 5
@@ -15,5 +15,13 @@ while n<length+1:
         newThread.start()
         print("Begun extracting frame " + str(n))
         n = n+1
-    else:
-        n=n
+
+n = 0
+
+while n < length+1:
+    path = modules.tmpFolder + modules.upscaleFolder + str(n) + ".png"
+    if threading.active_count()<threads:
+        newThread = threading.Thread(target=modules.frameUpscale,args=(path,4,),daemon=True)
+        newThread.start()
+        print("Begun upscaling frame " + str(n))
+        n = n+1
